a9145b080b2c20853e27165e06d6823307a9d0c7
Roland <superrid12@gmail.com>
2021-09-02 11:41:33 +0700

    first step

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// console.log("hello world!");
+
+// alert("hello world!!!");
+
+// let book = {
+//   one: "Java Script",
+//   two: 7,
+// };
+// console.log(book.one);
+// console.log(book.two);
+
+// let num = [1, 2, 3, 4, 5];
+// console.log(num[0]);
+let num1 = 5,
+  num2 = 10;
+// ++num1;
+// --num2;
+console.log(++num1, --num2);
