a2db89ee8787a033e688b435536d87c1892fc42a
Roland <superrid12@gmail.com>
2021-09-05 12:08:52 +0700

    next step

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
 "use strict";
 
-// console.log("hello world!");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-// alert("hello world!!!");
-
-// let book = {
-//   one: "Java Script",
-//   two: 7,
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
 // };
-// console.log(book.one);
-// console.log(book.two);
 
-// let num = [1, 2, 3, 4, 5];
-// console.log(num[0]);
-let num1 = 5,
-  num2 = 10;
-// ++num1;
-// --num2;
-console.log(++num1, --num2);
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//       b = prompt("На сколько оцените его?", ""),
+//       c = prompt("Один из последних просмотренных фильмов?", ""),
+//       d = prompt("На сколько оцените его?", "");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// const num = 50;
+// if (num == 49) {
+//   console.log('not 49');
+// } else if (num == 51) {
+//   console.log('not 51');
+// } else if (num > 52) {
+//   console.log('> 52');
+// } else if (num > 48) {
+//   console.log('> 48');
+// } else {
+//   console.log('not to day');
+// }
+
+// const num = 50;
+// switch(num) {
+//   case 49:
+//     console.log(49);
+//     break;
+//   case 51:
+//     console.log(51);
+//     break;
+//   case 50:
+//     console.log(50);
+//     break;
+//   default:
+//     console.log('not found');
+//     break;
+// }
+
+let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
